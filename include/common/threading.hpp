870775e885a5e0ec08327630e547dfaf6016d92a
allenbo <linjianfengqrh@gmail.com>
2014-11-11 00:09:43 -0800

    Add async method in threading

--- include/common/threading.hpp
+++ include/common/threading.hpp
@@ -80,7 +80,10 @@ class Condition {
 class Runable {
     public:
         virtual void run() = 0;
+        virtual ~Runable(){}
 };
+
+
 
 class Thread: public Runable {
     public:
@@ -137,6 +140,201 @@ class Thread: public Runable {
 
         bool _active;
         Lock* _lock;
+};
+
+class AsyncMethod {
+    public:
+        AsyncMethod(): _runable(NULL), _thread(NULL) {
+        }
+        void start_async(void (*fn) (void) ) {
+            typedef void (*FUNC) (void);
+            class VVRunable : public Runable {
+                public:
+                    VVRunable(FUNC fn) : _fn(fn) {
+                    }
+                    void run() {
+                        _fn();
+                    }
+                private:
+                    FUNC _fn;
+            };
+            _runable = new VVRunable(fn);
+            _thread = new Thread(*_runable);
+            _thread->start();
+        }
+
+        template<class R>
+        void start_async(R (*fn) (void), R* r) {
+            typedef R (*FUNC) (void);
+
+            class RVRunable : public Runable {
+                public:
+                    RVRunable(FUNC fn, R* r) : _fn(fn), _r(r){
+                    }
+                    void run() {
+                        *_r = _fn();
+                    }
+                private:
+                    FUNC _fn;
+                    R* _r;
+            };
+
+            _runable = new  RVRunable(fn, r);
+            _thread = new Thread(*_runable);
+            _thread->start();
+        }
+
+        template<class A1>
+        void start_async(void (*fn) (A1) , A1 a1) {
+            typedef void (*FUNC) (A1);
+
+            class VA1Runable : public Runable {
+                public:
+                    VA1Runable(FUNC fn, A1 a1) : _fn(fn), _a1(a1){
+                    }
+                    void run() {
+                        _fn(_a1);
+                    }
+                private:
+                    FUNC _fn;
+                    A1 _a1;
+            };
+
+            _runable = new  VA1Runable(fn, a1);
+            _thread = new Thread(*_runable);
+            _thread->start();
+        }
+        template<class A1, class R>
+        void start_async(R (*fn) (A1), A1 a1, R*r) {
+            typedef R (*FUNC) (A1);
+
+            class RA1Runable : public Runable {
+                public:
+                    RA1Runable(FUNC fn, A1 a1, R* r) : _fn(fn), _a1(a1), _r(r) {
+                    }
+
+                    void run() {
+                        *_r = _fn(_a1);
+                    }
+
+                private:
+                    FUNC _fn;
+                    A1 _a1;
+                    R* _r;
+            };
+
+            _runable = new RA1Runable(fn, a1, r);
+            _thread = new Thread(*_runable);
+            _thread->start();
+        }
+
+        template<class A1, class A2>
+        void start_async(void (*fn) (A1, A2) , A1 a1, A2 a2) {
+            typedef void (*FUNC) (A1, A2);
+
+            class VA2Runable : public Runable {
+                public:
+                    VA2Runable(FUNC fn, A1 a1, A2 a2) : _fn(fn), _a1(a1), _a2(a2) {
+                    }
+                    void run() {
+                        _fn(_a1, _a2);
+                    }
+                private:
+                    FUNC _fn;
+                    A1 _a1;
+                    A2 _a2;
+            };
+
+            _runable = new  VA2Runable(fn, a1, a2);
+            _thread = new Thread(*_runable);
+            _thread->start();
+        }
+        template<class A1, class A2, class R>
+        void start_async(R (*fn) (A1, A2), A1 a1, A2 a2, R*r) {
+            typedef R (*FUNC) (A1, A2);
+
+            class RA2Runable : public Runable {
+                public:
+                    RA2Runable(FUNC fn, A1 a1, A2 a2, R* r) : _fn(fn), _a1(a1), _a2(a2), _r(r) {
+                    }
+
+                    void run() {
+                        *_r = _fn(_a1, _a2);
+                    }
+
+                private:
+                    FUNC _fn;
+                    A1 _a1;
+                    A2 _a2;
+                    R* _r;
+            };
+
+            _runable = new RA2Runable(fn, a1, a2, r);
+            _thread = new Thread(*_runable);
+            _thread->start();
+        }
+
+        template<class A1, class A2, class A3>
+        void start_async(void (*fn) (A1, A2, A3) , A1 a1, A2 a2, A3 a3) {
+            typedef void (*FUNC) (A1, A2, A3);
+
+            class VA3Runable : public Runable {
+                public:
+                    VA3Runable(FUNC fn, A1 a1, A2 a2, A3 a3) : _fn(fn), _a1(a1), _a2(a2), _a3(a3){
+                    }
+                    void run() {
+                        _fn(_a1, _a2, _a3);
+                    }
+                private:
+                    FUNC _fn;
+                    A1 _a1;
+                    A2 _a2;
+                    A3 _a3;
+            };
+
+            _runable = new  VA3Runable(fn, a1, a2, a3);
+            _thread = new Thread(*_runable);
+            _thread->start();
+        }
+        template<class A1, class A2, class A3, class R>
+        void start_async(R (*fn) (A1, A2, A3), A1 a1, A2 a2, A3 a3, R*r) {
+            typedef R (*FUNC) (A1, A2, A3);
+
+            class RA3Runable : public Runable {
+                public:
+                    RA3Runable(FUNC fn, A1 a1, A2 a2, A3 a3, R* r) : _fn(fn), _a1(a1), _a2(a2), _a3(a3), _r(r) {
+                    }
+
+                    void run() {
+                        *_r = _fn(_a1, _a2, _a3);
+                    }
+
+                private:
+                    FUNC _fn;
+                    A1 _a1;
+                    A2 _a2;
+                    A3 _a3;
+                    R* _r;
+            };
+
+            _runable = new RA3Runable(fn, a1, a2, a3, r);
+            _thread = new Thread(*_runable);
+            _thread->start();
+        }
+
+        void wait() {
+          _thread->join();
+        }
+
+        ~AsyncMethod() {
+            _thread->join();
+            if (_runable) delete _runable;
+            if (_thread) delete _thread;
+        }
+
+    private:
+        Runable *_runable;
+        Thread *_thread;
 };
 
 }
--- test/test_threading.cpp
+++ test/test_threading.cpp
@@ -1,18 +1,146 @@
 #include "common/all.hpp"
-
 #include <stdio.h>
 
 using namespace COMMON;
 
 class MyThread: public Thread {
     public:
+        MyThread(int* pa, Mutex* m) : _pa(pa), _m(m){}
         void run() {
-            printf("In mythread\n");
+            ScopeLock _(_m);
+            *_pa += 1;
+            sleep(1);
         }
+    private:
+        Mutex* _m;
+        int* _pa;
 };
 
+static int global_int_value = 0;
+
+void vv_func() {
+    global_int_value++;
+}
+void test_async_vv() {
+    AsyncMethod async;
+    async.start_async(vv_func);
+    async.wait();
+    assert(global_int_value == 1);
+}
+
+int rv_func() {
+    return 1;
+}
+
+void test_async_rv() {
+    AsyncMethod async;
+    int a = 0;
+    async.start_async(rv_func, &a);
+    async.wait();
+    assert(a == 1);
+}
+
+void va1_func(int a) {
+    global_int_value = a;
+}
+
+void test_async_va1() {
+    int a = 10;
+    AsyncMethod async;
+    async.start_async(va1_func, a);
+    async.wait();
+    assert(global_int_value == a);
+}
+
+int ra1_func(int a) {
+    return a * global_int_value;
+}
+
+void test_async_ra1() {
+    int a = 5;
+    int r = 0;
+    AsyncMethod async;
+    async.start_async(ra1_func, a, &r);
+    async.wait();
+    assert(global_int_value * a == r);
+}
+
+void va2_func(int a, int b) {
+    global_int_value = a + b;
+}
+
+void test_async_va2() {
+    int a = 1, b = 2;
+    AsyncMethod async;
+    async.start_async(va2_func, a, b);
+    async.wait();
+    assert(global_int_value == a + b);
+}
+
+int ra2_func(int a , int b) {
+    return a + b;
+}
+
+void test_async_ra2() {
+    int a = 1, b = 2;
+    int r = 0;
+    AsyncMethod async;
+    async.start_async(ra2_func, a, b, &r);
+    async.wait();
+    assert(r == a + b);
+}
+
+void va3_func(int a, int b, int c) {
+    global_int_value = a + b + c;
+}
+
+void test_async_va3() {
+  int a = 1, b = 2, c = 3;
+    AsyncMethod async;
+    async.start_async(va3_func, a, b, c);
+    async.wait();
+    assert(global_int_value == a + b + c);
+}
+
+int ra3_func(int a, int b, int c){
+    return a + b + c;
+}
+
+void test_async_ra3(){
+    int a = 1, b = 2, c = 3;
+    int r = 0;
+
+    AsyncMethod async;
+    async.start_async(ra3_func, a, b, c, &r);
+    async.wait();
+    assert(r == a + b + c);
+}
+
+void test_async() {
+  test_async_vv();
+  test_async_rv();
+  test_async_va1();
+  test_async_ra1();
+  test_async_va2();
+  test_async_ra2();
+  test_async_va3();
+  test_async_ra3();
+}
 int main() {
-    MyThread thread[10];
+    int a = 0;
+    Mutex m;
+    MyThread thread[10] = {
+        MyThread(&a, &m),
+        MyThread(&a, &m),
+        MyThread(&a, &m),
+        MyThread(&a, &m),
+        MyThread(&a, &m),
+        MyThread(&a, &m),
+        MyThread(&a, &m),
+        MyThread(&a, &m),
+        MyThread(&a, &m),
+        MyThread(&a, &m)
+    };
     for (int i = 0; i < 10; i ++ ) {
         thread[i].start();
     }
@@ -20,4 +148,6 @@ int main() {
     for (int i = 0; i < 10; i ++) {
         thread[i].join();
     }
+    assert(a == 10);
+    test_async();
 }
